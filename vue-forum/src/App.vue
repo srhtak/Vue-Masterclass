14f8d0348d5a720063ac694c4654bdc9cd627fee
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-04-24 13:43:37 +0300

    Created thread page

--- vue-forum/src/App.vue
+++ vue-forum/src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <div class="container">
-    <page-home/>
+    <router-view/>
   </div>
 
 </template>
 
 <script>
-import PageHome from "@/components/PageHome";
 export default {
   name: 'App',
-  components: {
-    PageHome,
-  },
 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/style.css";
 </style>
--- vue-forum/src/main.js
+++ vue-forum/src/main.js
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter,createWebHistory} from  'vue-router'
+import PageHome from "@/components/PageHome";
+import PageThreadShow from "@/components/PageThreadShow";
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: PageHome
+    },
+    {
+        path: '/thread/:id',
+        name: 'ThreadShow',
+        component: PageThreadShow
+    },
+
+]
+
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
+
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
