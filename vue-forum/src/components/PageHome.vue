f9af69c95ab7d75aab62514c3e0c4de43e6c7c64
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-04-24 13:12:34 +0300

    add style.css file and modified Home Page

--- vue-forum/src/components/PageHome.vue
+++ vue-forum/src/components/PageHome.vue
@@ -1,13 +1,91 @@
 
 <script>
+import sourceData from "@/data.json"
 export default {
-
+  data () {
+    return {
+      threads : sourceData.threads,
+      posts : sourceData.posts,
+      users: sourceData.users,
+    }
+  },
+  methods:{
+    postById(postId){
+      return this.posts.find(p => p.id === postId)
+    },
+    userById(userId){
+      return this.users.find(u =>u.id === userId)
+    }
+  }
 }
 </script>
 
 
 <template>
-  <h1>Serhat</h1>
+  <div class="col-large push-top" v-for="thread in threads" :key="thread.id">
+
+    <h1>{{ thread.title}}</h1>
+
+
+
+    <div class="post-list">
+
+
+      <div class="post"
+           v-for="postId in thread.posts"
+           :key="postId">
+
+        <div class="user-info">
+          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
+
+          <a href="#">
+            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+
+
+
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{postById(postId).text}}
+            </p>
+          </div>
+        </div>
+
+
+
+        <div class="post-date text-faded">
+          {{postById(postId).publishedAt}}
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+
+  <h1>Home Page</h1>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{thread.title}}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
