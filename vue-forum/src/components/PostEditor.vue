6e0f57f349bae1ae6227132e747bfa6667903f79
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-04-24 20:25:26 +0300

    post method added

---
+++ vue-forum/src/components/PostEditor.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "PostEditor",
+  data () {
+    return {
+      newPostText: '',
+    }
+  },
+  methods:{
+    addPost(){
+      const postId = 'qqqq' + Math.random()
+      const post = {
+        id:postId,
+        text:this.newPostText,
+        publishedAt:Math.floor(Date.now()/1000),
+        threadId: this.id,
+        userId:'7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+      }
+      this.posts.push(post)
+      this.thread.posts.push(postId)
+      this.newPostText = ''
+    }
+  }
+}
+</script>
+
+<template>
+
+  <div class="col-full">
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea v-model="newPostText" name="" id="" cols="50" rows="10" class="form-input"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit Post</button>
+      </div>
+    </form>
+  </div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
